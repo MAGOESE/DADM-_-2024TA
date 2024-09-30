5b233e8471208c8ec80147ead68d33a8ba81f5ec
MAGOESE <llaveverde37@gmail.com>
2024-09-30 18:18:03 +0000

    Reto Form

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,41 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+  import { ref } from 'vue';
+  // Modelo
+  const header = ref('App lista de compras');
+  const items = ref([ 
+    {id: '0', label: '10 Bolillos'},
+    {id: '1', label: '1 Lata Frijoles'},
+    {id: '2', label: '1 Chelas'},
+    {id: '3', label: '1 Nutella'},
+  ]);
+  const newItem = ref('');
+  const newItemPriority = ref('low');
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>
+    <i class="material-icons shopping-cart-icon">local_mall</i>
+    {{ header }}
+  </h1>
+  <input v-model="newItem" type="text"
+  placeholder="Agregar Articulo">
+ <!-- Radio Button -->
+  <label>
+    <input type="radio" value="low" v-model="newItemPriority">
+    Baja
+  </label>
+  <label>
+    <input type="radio" value="high" v-model="newItemPriority">
+    Alta
+  </label>
+{{ newItemPriority == 'low' ? '🧊' : '🔥' }}
+  <ul>
+    <li v-for="item in items" :key="item.id"> 🛍️ {{ item.label }} </li>
+  </ul>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.shopping-cart-icon {
+  font-size: 2rem;
 }
 </style>
